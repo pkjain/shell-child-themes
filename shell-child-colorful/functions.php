cc8253462229081f49e59693bd0e5a4797eb77a0
Parveen Kumar <pkjain@gmail.com>
2013-03-11 09:47:09 -0400

    remove thumbnail support

--- shell-child-colorful/functions.php
+++ shell-child-colorful/functions.php
@@ -3,6 +3,7 @@
 // Don't display breadcrumb-trail
 function scc_after_setup_theme_callback() {
   remove_theme_support('breadcrumb-trail');
+  remove_theme_support('get-the-image');
 }
 // Priority 11 to ensure it is executed in the end
 add_action('after_setup_theme', 'scc_after_setup_theme_callback', 11);
