a3c90db62fdf64e3862888a1aa5f8318b2498891
Parveen Kumar <pkjain@gmail.com>
2013-03-16 09:30:23 -0400

    removed scc_footer_content_code

--- shell-child-colorful/functions.php
+++ shell-child-colorful/functions.php
@@ -18,7 +18,6 @@ add_action('wp_enqueue_scripts', 'scc_enqueue_dequeue_scripts', 1000);
 function scc_after_setup_theme_callback() {
   $prefix = hybrid_get_prefix();
   add_action( "{$prefix}_byline", 'scc_byline_code');
-  add_action( "{$prefix}_footer_content", 'scc_footer_content_code');
   add_action( "{$prefix}_entry_meta", 'scc_entry_meta_code');
 
   // theme support add/remove
@@ -32,13 +31,6 @@ add_action('after_setup_theme', 'scc_after_setup_theme_callback', 11);
 /* Display only date in byline */
 function scc_byline_code() {
   echo do_shortcode('<div class="byline">[entry-published] [entry-edit-link before=" | "]</div>');
-}
-
-
-/* child-link as well in footer */
-function scc_footer_content_code() {
-  $content = '<p class="copyright">' . __( 'Copyright &#169; [the-year] [site-link].', 'shell' ) . '</p>' . "\n\n" . '<p class="credit">' . __( 'Powered by [wp-link], [theme-link], and [child-link].', 'shell' ) . '</p>';
-  echo do_shortcode($content);
 }
 
 
