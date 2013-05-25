da40d49e57e2a20c2c507d49e7217e18a9ab7361
pkjain <Parveen Kumar>
2013-05-25 08:10:59 +0000

    prev next style

--- shell-child-colorful/loop-nav/singular.php
+++ shell-child-colorful/loop-nav/singular.php
@@ -8,7 +8,7 @@
 ?>
 	<?php if ( is_singular( 'post' ) ) : ?>
 		<div class="loop-nav">
-			<?php previous_post_link( '<div class="previous"><< ' . __( '%link', 'shell' ) . '</div>', '%title' ); ?>
-			<?php next_post_link( '<div class="next">' . __( '%link', 'shell' ) . ' >></div>', '%title' ); ?>
+			<?php previous_post_link( '<div class="previous"><div style="float:left;padding-right:5px;"><<</div><div>' . __( '%link', 'shell' ) . '</div></div>', '%title' ); ?>
+			<?php next_post_link( '<div class="next"><div style="float:right;padding-left:5px;">>></div><div>' . __( '%link', 'shell' ) . '</div></div>', '%title' ); ?>
 		</div><!-- .loop-nav -->
 	<?php endif; ?>
