0a45a77544533f26cd8b0e5a8bcbd8fbb0059774
Parveen Kumar <pkjain@gmail.com>
2016-01-02 15:22:12 +0000

    disable loop navigation - (need to remove code also in a better way)

--- shell-child-colorful/loop-nav/singular.php
+++ shell-child-colorful/loop-nav/singular.php
@@ -6,7 +6,7 @@
  * @subpackage Template
  */
 ?>
-	<?php if ( is_singular( 'post' ) ) : ?>
+	<?php if (false && is_singular( 'post' ) ) : ?>
 		<div class="loop-nav">
 			<?php previous_post_link( '<div class="previousicon"><span><<<span></div><div class="previous">' . __( '%link', 'shell' ) . '</div>', '%title' ); ?>
 			<?php next_post_link( '<div class="next">' . __( '%link', 'shell' ) . '</div><div class="nexticon"><span>>></span></div>', '%title' ); ?>
